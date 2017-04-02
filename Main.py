f571be4e7906ff8b9905130e64ad9a6d0e409478
jay-chakalasiya <chakalasiyajay00@gmail.com>
2017-04-02 11:34:14 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,22 +1,26 @@
-import pandas as pd
-
-from Dataset import GENERATEFEED
-from Indicators import RSI,EMA
-
-
-companies_list = ["M&M","MTNL","ONGC","RELINFRA","SAIL","SBI","SUNPHARMA","TATAM","TATAP","WIPRO"]
-#"BHEL","CIPLA","GAIL","HDFC","HINCONST","HINDALCO","IOC","ITC","L&T","LUPIN",
+# -*- coding: utf-8 -*-
 """
+Created on Sun Feb 12 22:41:14 2017
+
+@author: JAY CHAKALASIYA
+"""
+
+import pandas as pd
+#from Indicators import RSI,AROON, STOS, OBV
+from Dataset import GENERATEFEED
+
+#"BHEL","BSE","CIPLA","GAIL",
+companies_list = ["HDFC","HINCONST","HINDALCO","IOC","ITC","L&T","LUPIN","M&M","MTNL","NSE_from2007","ONGC","RELINFRA","SAIL","SBI","SUNPHARMA","TATAM","TATAP","WIPRO"]
+
+
 i=0
 while i < len(companies_list):
     file = pd.read_csv("IndData/"+companies_list[i]+".csv")
-    feed = GENERATEFEED(file[0:2000])
-    feed.to_csv("PreProcessed/"+companies_list[i]+".csv")
+    feed = GENERATEFEED(file)
+    feed.to_csv("Processed2.0/"+companies_list[i]+".csv")
     print(i)
-    i=i+1
-"""
-file = pd.read_csv("IndData/M&M.csv")
-test = RSI(file)
-file["RSI"]=test
-print(file)
-#file["RSI"]=test
+    i+=1
+
+
+
+
